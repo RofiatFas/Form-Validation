a5c79dbf057ded64b4b74bad3fa9e1623724eb35
Rofiat Fasasi <usmanfasasirofiat@gmail.com>
2022-02-12 16:26:34 +0100

    added new texts

--- index.js
+++ index.js
@@ -15,8 +15,38 @@ let firstname = id(`firstname`),
      failureIcon = classes(`failure-icon`);
 
 // Target form and submit event listner
+// let form = document.querySelector("form")
 form.addEventListner(`submit`, (e) =>{
     e.preventDefault();
+
+    engine(firstname, 0, "Firstname cannot be blank");
+    engine(lastname, 1, "Last name cannot be blank");
+    engine(username, 2, "Username cannot be blank");
+    engine(email, 3, "email cannot be blamk");
+    engine(password, 4, "Password must consist of numbers,letters and symbols")
 });
 
-Create 
+// Create a function named engine which will have three arguments
+// let engine = (id, serial, message) => {}
+// id- will target our id;
+// serial- will target classes(error class, success and failure)
+// message- will print a message inside our .error class
+// And create an if Statement
+
+let engine = (id, serial, message) => {
+    if(id.value.trim() === ""){
+        errorMsg[serial].innerHTML = message;
+        id.style.border = "2px solid red";
+
+        // icons
+        failureIcon[serial].style.opacity = "0";
+        successIcon[serial].style.opacity = "0";
+    }else{
+        errorMsg[serial].innerHTML = "";
+        id.style.border = `2px solid green`;
+
+        // icons
+        failureIcon[serial].style.opacity = "0";
+        successIcon[serial].style.opacity = "1";
+    }
+}
